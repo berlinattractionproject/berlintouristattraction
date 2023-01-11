c05f3d195bed93dfd2d57f2fa48db113f6723794
David Amberg <david.amberg@ymail.com>
2023-01-11 14:28:32 +0100

    added styling to index

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,4 @@
 window.addEventListener('load', () => {
-  let geocoder =  new google.maps.Geocoder();
 
     const berlin = {
       lat: 52.520008,
@@ -10,26 +9,61 @@ window.addEventListener('load', () => {
       zoom: 13,
       center: berlin
     });
-    var marker = new google.maps.Marker({
-      position: berlin,
-      map: map
-  });
-  const address = "Potsdamer Platz 50, Berlin"
-  function codeAddress(address) {
 
-    geocoder.geocode({ 'address': address }, function (results, status) {
-        if (status == 'OK') {
-            console.log(address)
-                var marker = new google.maps.Marker({
-                position: address,
-                map: map
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
+    function getPlaces() {
+  axios
+    .get('/maps/api')
+    .then(response => {
+      console.log(response.data.places)
+      // placePlaces(response.data.places);
+      response.data.places.forEach((place)=> {
+        const {address,zipCode,name} = place
+        const fullAddress = `${address},${zipCode}`
+        console.log(name)
+
+        geocodeAddress(geocoder, map, fullAddress,name)
+
+      })
+    })
+    .catch(error => {
+      console.log(error);
     });
 }
-codeAddress(address);
+
+const geocoder = new google.maps.Geocoder();
+
+
+function geocodeAddress(geocoder, resultsMap,fullAddress,placeName) {
+  const address = fullAddress
+ console.log(placeName)
+  geocoder.geocode({ address: address }, (results, status) => {
+    if (status === 'OK') {
+    console.log(results[0].geometry.location.lat())
+      let marker = new google.maps.Marker({
+                  map: map,
+                  position: results[0].geometry.location,
+                  label: {
+                    color: 'red',
+                    fontWeight: 'bold',
+                    text: placeName,
+                    fontSize: '24px',
+                },
+                icon: {
+                  // labelOrigin: new google.maps.Point(11, 50),
+                  url: 'default_marker.png',
+                  // size: new google.maps.Size(22, 40),
+                  // origin: new google.maps.Point(0, 0),
+                  // anchor: new google.maps.Point(11, 40),
+                }});
+    } else {
+      console.log(`Geocode was not successful for the following reason: ${status}`);
+    }
+  });
+}
+
+
+
+getPlaces();
+
 
   });
-
